264b977831cb44e90bd8690e55c8df6f40ac2353
Deadlive1 <legendary2002k@gmail.com>
2022-06-24 00:28:01 +0300

    /

---
+++ _nuxt/static/1656019569/courses/1/payload.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("/courses/1", {data:[{}],fetch:{},mutations:[]});
---
+++ _nuxt/static/1656019569/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002Fabout","\u002Flogin","\u002Fcourses","\u002F","\u002Fcourses\u002F0","\u002Fcourses\u002F1","\u002Fcourses\u002F2","\u002Fcourses\u002F3","\u002Fcourses\u002F4","\u002Fcourses\u002F5","\u002Fcourses\u002Flessons\u002F3","\u002Fcourses\u002Flessons\u002F2","\u002Fcourses\u002Flessons\u002F1"]})
